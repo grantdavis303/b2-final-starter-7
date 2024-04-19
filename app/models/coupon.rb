d5b9266194c6dba776188fea3a8b76ad3c3444a2
Grant Davis <grantdavis303@gmail.com>
2024-04-19 08:45:25 -0600

    Fix Validations for Coupon

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,14 +2,15 @@ class Coupon < ApplicationRecord
   validates_presence_of :name,
                         :code,
                         :amount,
-                        :type,
+                        :amount_type,
                         :status,
-                        :merchant_id,
-                        :invoice_id
+                        :merchant_id
+
+  validates :invoice_id, presence: true, allow_blank: true
   
   belongs_to :merchant
   belongs_to :invoice, optional: true
 
-  enum type: ["dollar", "percent"]
-  enum status: ["enabled", "disabled"]
+  enum amount_type: [:dollar, :percent]
+  enum status: [:enabled, :disabled]
 end
