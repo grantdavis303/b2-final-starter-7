20a61b884621426324c35e0d49c75f9ffd89d93c
Grant Davis <grantdavis303@gmail.com>
2024-04-19 16:37:52 -0600

    User Story 2 Complete - No Sad Path

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -7,4 +7,23 @@ class CouponsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @coupon = Coupon.find(params[:id])
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    new_coupon = @merchant.coupons.new(coupon_params)
+    new_coupon.save # implement sad path testing
+
+    redirect_to merchant_coupons_path(@merchant)
+  end
+
+  private
+  def coupon_params
+    params
+    .permit(:name, :code, :amount, :amount_type)
+    .with_defaults(status: :disabled)
+  end
 end
