dd7ef329613f5fa25d72f7f1871e6404c9072a36
Grant Davis <grantdavis303@gmail.com>
2024-04-19 08:45:00 -0600

    Change Validation Tests

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -9,10 +9,10 @@ RSpec.describe Coupon, type: :model do
   describe "validations" do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:code) }
-    it { should validate_presence_of(:amount) }
-    it { should validate_presence_of(:type) }
+    it { should validate_presence_of(:amount_type) }
+    it { should validate_presence_of(:amount_type) }
     it { should validate_presence_of(:status) }
     it { should validate_presence_of(:merchant_id) }
-    it { should validate_presence_of(:invoice_id) }
+    it { should allow_value("", nil).for(:invoice_id) }
   end
 end
