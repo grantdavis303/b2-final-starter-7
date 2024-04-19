91be0cb859767d58b8975fa62944d4fe8c8109df
Grant Davis <grantdavis303@gmail.com>
2024-04-19 07:53:10 -0600

    Set up initial folders for Coupons

---
+++ app/models/coupon.rb
@@ -0,0 +1,15 @@
+class Coupon < ApplicationRecord
+  validates_presence_of :name,
+                        :code,
+                        :amount,
+                        :type,
+                        :status,
+                        :merchant_id,
+                        :invoice_id
+  
+  belongs_to :merchant
+  belongs_to :invoice, optional: true
+
+  enum type: ["percent", "dollar"]
+  enum status: ["enabled", "disabled"]
+end
