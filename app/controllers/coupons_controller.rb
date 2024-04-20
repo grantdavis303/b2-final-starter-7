c27266050fdbc4f874811518e5686d13fa67bfe1
Grant Davis <grantdavis303@gmail.com>
2024-04-19 22:04:48 -0600

    Begin Sad Path Tests

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -15,9 +15,22 @@ class CouponsController < ApplicationController
   def create
     @merchant = Merchant.find(params[:merchant_id])
     new_coupon = @merchant.coupons.new(coupon_params)
-    new_coupon.save # implement sad path testing
 
-    redirect_to merchant_coupons_path(@merchant)
+    if new_coupon.save # implement sad path testing
+      redirect_to merchant_coupons_path(@merchant)
+    else
+      flash[:notice] = "Coupon not created: Information missing. Please fill in any empty fields."
+      render :new
+    end
+
+    # if validates_uniqueness_of :number == "true"
+    #   puts "this value is unique and should be added to the DB"
+    # else 
+    #   puts "this value is not unique and should not be added to the DB"
+    # end
+
+    # elsif params[:code]
+    # elsif @merchant.active_coupons.count >= 5
   end
 
   private
