4dd6762d32d25ee48cb02a94cb223cfec017d1ab
Grant Davis <grantdavis303@gmail.com>
2024-04-20 17:30:46 -0600

    Add Sad Path Testing for Coupons

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -16,30 +16,12 @@ class CouponsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     new_coupon = @merchant.coupons.new(coupon_params)
 
-    if new_coupon.save # implement sad path testing
+    if new_coupon.save
       redirect_to merchant_coupons_path(@merchant)
+    else
+      flash.now[:notice] = new_coupon.errors.full_messages.to_sentence
+      render :new
     end
-
-    # elsif params[:amount].class != Integer
-    #   flash[:notice] = "Please enter a number for amount."
-    #   render :new
-    # elsif @merchant.active_coupons.count >= 5 
-    #   flash[:notice] = "Coupon not created: Active Coupons max amount reached. Please deactivate other coupons to create another."
-    #   render :new     
-    # else
-    #   flash[:notice] = "Coupon not created: Information missing. Please fill in any empty fields."
-    #   render :new
-    # end
-
-    # How to get flash to go away?
-
-    # if validates_uniqueness_of :number == "true"
-    #   puts "this value is unique and should be added to the DB"
-    # else 
-    #   puts "this value is not unique and should not be added to the DB"
-    # end
-
-    # elsif params[:code]
   end
 
   def update
