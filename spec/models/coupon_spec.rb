6a509addfae2941e20f6c963b9d5536e0dcab06c
Grant Davis <grantdavis303@gmail.com>
2024-04-21 13:18:15 -0600

    Get Validation for Uniqueness to Pass

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -5,15 +5,18 @@ RSpec.describe Coupon, type: :model do
     it { should belong_to(:merchant) }
   end
 
-  describe "validations" do  
+  describe "validations" do
+    subject {
+      @merchant1 = Merchant.create!(name: "Hair Care")
+      @coupon_1 = Coupon.new(merchant_id: @merchant1.id)
+    }
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:code) }  
     it { should validate_presence_of(:amount) }
     it { should validate_presence_of(:amount_type) }
     it { should validate_presence_of(:status) }
     it { should validate_presence_of(:merchant_id) }
-    # validate uniqueness of code
-    #it { should validate_uniqueness_of(:code).on(:create) }
+    it { should validate_uniqueness_of(:code) }
     it { should validate_numericality_of(:amount) }
   end
 
