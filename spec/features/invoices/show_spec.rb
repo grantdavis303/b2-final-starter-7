e132c1c9672acf59f8492333c9ef118d1820033c
Grant Davis <grantdavis303@gmail.com>
2024-04-21 10:32:41 -0600

    Add User Story 7 Test and Sad Paths

--- spec/features/invoices/show_spec.rb
+++ spec/features/invoices/show_spec.rb
@@ -22,10 +22,14 @@ RSpec.describe "invoices show" do
     @customer_5 = Customer.create!(first_name: "Sylvester", last_name: "Nader")
     @customer_6 = Customer.create!(first_name: "Herber", last_name: "Kuhn")
 
-    @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
+    @coupon = Coupon.create!(name: "Coupon 1", code: "$1_OFF_ALL", amount: 1, amount_type: 0, status: 0, merchant_id: @merchant1.id )
+    @coupon1 = Coupon.create!(name: "Coupon 2", code: "$50_OFF_ALL", amount: 50, amount_type: 0, status: 0, merchant_id: @merchant1.id )
+    @coupon2 = Coupon.create!(name: "Coupon 3", code: "125%_OFF", amount: 100, amount_type: 1, status: 0, merchant_id: @merchant1.id )
+
+    @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09", coupon_id: @coupon.id)
     @invoice_2 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-28 14:54:09")
-    @invoice_3 = Invoice.create!(customer_id: @customer_2.id, status: 2)
-    @invoice_4 = Invoice.create!(customer_id: @customer_3.id, status: 2)
+    @invoice_3 = Invoice.create!(customer_id: @customer_2.id, status: 2, created_at: "2012-03-27 14:54:09", coupon_id: @coupon1.id)
+    @invoice_4 = Invoice.create!(customer_id: @customer_3.id, status: 2, created_at: "2012-03-27 14:54:09", coupon_id: @coupon2.id)
     @invoice_5 = Invoice.create!(customer_id: @customer_4.id, status: 2)
     @invoice_6 = Invoice.create!(customer_id: @customer_5.id, status: 2)
     @invoice_7 = Invoice.create!(customer_id: @customer_6.id, status: 2)
@@ -34,8 +38,8 @@ RSpec.describe "invoices show" do
 
     @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 9, unit_price: 10, status: 2)
     @ii_2 = InvoiceItem.create!(invoice_id: @invoice_2.id, item_id: @item_1.id, quantity: 1, unit_price: 10, status: 2)
-    @ii_3 = InvoiceItem.create!(invoice_id: @invoice_3.id, item_id: @item_2.id, quantity: 2, unit_price: 8, status: 2)
-    @ii_4 = InvoiceItem.create!(invoice_id: @invoice_4.id, item_id: @item_3.id, quantity: 3, unit_price: 5, status: 1)
+    @ii_3 = InvoiceItem.create!(invoice_id: @invoice_3.id, item_id: @item_2.id, quantity: 50, unit_price: 8, status: 2)
+    @ii_4 = InvoiceItem.create!(invoice_id: @invoice_4.id, item_id: @item_3.id, quantity: 50, unit_price: 8, status: 1)
     @ii_6 = InvoiceItem.create!(invoice_id: @invoice_5.id, item_id: @item_4.id, quantity: 1, unit_price: 1, status: 1)
     @ii_7 = InvoiceItem.create!(invoice_id: @invoice_6.id, item_id: @item_7.id, quantity: 1, unit_price: 3, status: 1)
     @ii_8 = InvoiceItem.create!(invoice_id: @invoice_7.id, item_id: @item_8.id, quantity: 1, unit_price: 5, status: 1)
@@ -74,15 +78,14 @@ RSpec.describe "invoices show" do
 
     expect(page).to have_content(@item_1.name)
     expect(page).to have_content(@ii_1.quantity)
-    expect(page).to have_content(@ii_1.unit_price)
+    expect(page).to have_content("$0.10") # expect(page).to have_content(@ii_1.unit_price)
     expect(page).to_not have_content(@ii_4.unit_price)
-
   end
 
   it "shows the total revenue for this invoice" do
     visit merchant_invoice_path(@merchant1, @invoice_1)
 
-    expect(page).to have_content(@invoice_1.total_revenue)
+    expect(page).to have_content("$1.62") # expect(page).to have_content(@invoice_1.total_revenue)
   end
 
   it "shows a select field to update the invoice status" do
@@ -100,4 +103,47 @@ RSpec.describe "invoices show" do
     end
   end
 
+  # User Story 7
+  it "shows the subtotal and grand total before a discount is applied" do
+    # As a merchant, when I visit one of my merchant invoice show pages
+    visit merchant_invoice_path(@merchant1, @invoice_1)
+
+    # I see the subtotal for my merchant from this invoice (that is, the total that does not include coupon discounts)
+    # And I see the grand total revenue after the discount was applied
+    within(".invoice_totals") do
+      expect(page).to have_content("Subtotal: $1.62")
+      expect(page).to have_content("Grand Total: $0.62")
+    end
+  
+    # And I see the name and code of the coupon used as a link to that coupon's show page.
+    within(".applied_coupon_info") do
+      expect(page).to have_link(@coupon.name)
+      expect(page).to have_content(@coupon.name)
+      expect(page).to have_content(@coupon.code)
+    end
+
+    within(".applied_coupon_info") do
+      click_link(@coupon.name)
+      expect(current_path).to eq(merchant_coupon_path(@merchant1, @coupon))
+    end
+  end
+
+  # Sad Paths
+  it "discount is larger than total revenue - dollars" do
+    visit merchant_invoice_path(@merchant1, @invoice_3)
+
+    within(".invoice_totals") do
+      expect(page).to have_content("Subtotal: $4.00")
+      expect(page).to have_content("Grand Total: $0.00")
+    end
+  end
+
+  it "discount is larger than total revenue - percentage" do
+    visit merchant_invoice_path(@merchant1, @invoice_4)
+
+    within(".invoice_totals") do
+      expect(page).to have_content("Subtotal: $4.00")
+      expect(page).to have_content("Grand Total: $0.00")
+    end
+  end
 end
